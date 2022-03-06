1cfbcb7b497990038c9a9d0603ea682f13236700
Omar Alashqar <oaaalash@uwaterloo.ca>
2022-03-06 01:39:38 -0500

    fixes migration

--- server/src/startup/db.ts
+++ server/src/startup/db.ts
@@ -1,9 +1,12 @@
 import { createConnection } from "typeorm";
 import path from "path";
 import { __prod__ } from "../constants";
+
 import { Post } from "../entities/Post";
 import { User } from "../entities/User";
 import { Vote } from "../entities/Vote";
+
+import { Initial1598727826193 } from "../migrations/1598727826193-Initial";
 
 export const db = async () => {
   return createConnection({
@@ -12,6 +15,6 @@ export const db = async () => {
     logging: !__prod__,
     synchronize: !__prod__,
     entities: [Post, User, Vote],
-    migrations: [path.join(__dirname, "./migrations/*")],
+    migrations: [Initial1598727826193],
   });
 };
